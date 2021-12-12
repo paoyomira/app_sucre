fd51839f284f42b9853e3bc42f28f18fd7c9c4bc
Eduardo Acosta <aczanied@gmail.com>
2021-12-12 17:05:52 -0500

    Megre

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,9 +4,28 @@ export 'package:app_sucre/widgets/menu_widget.dart';
 
 // Forms
 export 'package:app_sucre/widgets/forms/login_form_widget.dart';
+export 'package:app_sucre/widgets/forms/register_form_widget.dart';
 
 // Buttons
+export 'package:app_sucre/widgets/buttons/register_button_widget.dart';
+export 'package:app_sucre/widgets/buttons/login_button_widget.dart';
+export 'package:app_sucre/widgets/buttons/user_register_button_widget.dart';
+export 'package:app_sucre/widgets/buttons/user_login_widget.dart';
+export 'package:app_sucre/widgets/buttons/evidence_photos_button_widget.dart';
 
 // Inputs
 export 'package:app_sucre/widgets/inputs/password_input_widget.dart';
 export 'package:app_sucre/widgets/inputs/text_input_widget.dart';
+export 'package:app_sucre/widgets/inputs/incidence_description_widget.dart';
+export 'package:app_sucre/widgets/inputs/incidence_detail_widget.dart';
+
+// Cards
+export 'package:app_sucre/widgets/cards/home_card_widget.dart';
+export 'package:app_sucre/widgets/cards/incident_card_widget.dart';
+export 'package:app_sucre/widgets/cards/latest_reports_card_widget.dart';
+
+// Bars
+export 'package:app_sucre/widgets/bars/search_bar_widget.dart';
+
+// Texts
+export 'package:app_sucre/widgets/texts/create_account_widget.dart';
